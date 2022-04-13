0ea04dc42153254c6e7ec77a62ad11bf992aedab
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-04-13 20:17:49 +0300

    created gitignore file and added virtual folder and .env file

--- auth/settings.py
+++ auth/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%4)ag-j=i%1-w+3y9!io%lvtqg=c2c5&vj&b)qm2tc&h+#xfv$'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
