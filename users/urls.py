7be20fc43fe862534b74b5cae41a515a740d0532
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-04-13 20:10:55 +0300

    resolved bugs

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,5 @@ urlpatterns =[
     path('register', RegisterView.as_view()),
     path('login', loginView.as_view()),
     path('user', UserView.as_view()),
-    path(logout, logoutView.as_view()),
+    path('logout', logoutView.as_view()),
 ]
